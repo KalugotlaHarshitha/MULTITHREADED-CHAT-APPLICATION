d10db91af05d08a619f71d4743d7edfdfd1decd4
KalugotlaHarshitha <kalugotlaharshitha2912@gmail.com>
2025-03-02 14:37:02 +0530

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,50 +1,57 @@
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ChatServer {
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(12345)) {
+   private static Set<ClientHandler> clients = new HashSet();
+
+   public ChatServer() {
+   }
+
+   public static void main(String[] var0) {
+      try {
+         ServerSocket var1 = new ServerSocket(12345);
+
+         try {
             System.out.println("Chat Server started...");
 
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected.");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-
-            String clientMessage;
-            while (true) {
-                clientMessage = reader.readLine(); 
-                if (clientMessage == null || clientMessage.equalsIgnoreCase("exit")) {
-                    System.out.println("Client disconnected. Closing server...");
-                    break;
-                }
-
-                System.out.println("Received from client: " + clientMessage);
-
-                
-                String serverResponse;
-                if(clientMessage.equalsIgnoreCase("hello")){
-                    serverResponse="Hii, how can I help you";
-                }
-                else if (clientMessage.equalsIgnoreCase("how are you")) {
-                    serverResponse = "Fine";
-                } 
-                else if(clientMessage.equalsIgnoreCase("what's up")){
-                    serverResponse="Nothing,how about you";
-                }
-                else {
-                    serverResponse = "Server: " + clientMessage; 
-                }
-
-                writer.println(serverResponse); 
-                System.out.println("Sent to client: " + serverResponse);
+            while(true) {
+               Socket var2 = var1.accept();
+               System.out.println("New client connected!");
+               ClientHandler var3 = new ClientHandler(var2);
+               clients.add(var3);
+               (new Thread(var3)).start();
+            }
+         } catch (Throwable var5) {
+            try {
+               var1.close();
+            } catch (Throwable var4) {
+               var5.addSuppressed(var4);
             }
 
-            socket.close();
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+            throw var5;
+         }
+      } catch (IOException var6) {
+         var6.printStackTrace();
+      }
+   }
+
+   public static void broadcast(String var0, ClientHandler var1) {
+      Iterator var2 = clients.iterator();
+
+      while(var2.hasNext()) {
+         ClientHandler var3 = (ClientHandler)var2.next();
+         if (var3 != var1) {
+            var3.sendMessage(var0);
+         }
+      }
+
+   }
+
+   public static void removeClient(ClientHandler var0) {
+      clients.remove(var0);
+   }
 }
